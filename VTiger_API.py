4f8641da307b643201e5e0b0944fc23b658d0cd2
Roovy Shapiro <roovyshapiro@gmail.com>
2019-08-19 16:35:37 -0400

    Created VTiger_API.py
    Initial commit of API testing file

---
+++ VTiger_API.py
@@ -0,0 +1,23 @@
+#https://www.vtiger.com/docs/rest-api-for-vtiger
+import requests, json
+
+username ='(USERNAME)'
+access_key = '(ACCESS KEY)'
+host = 'https://(MYURL).vtiger.com/restapi/v1/vtiger/default/'
+
+#Get information about me
+#url = f"{host}/me"
+
+#Query Case Module and order by Modified Time
+#Returns the first 100 cases
+url = f"{host}/query?query=Select * FROM Cases order by modifiedtime;"
+
+r = requests.get(url, auth=(username, access_key))
+
+print(f"The status code is: {r.status_code}\n")
+
+#Write JSON data to a file
+r_text = json.loads(r.text)
+my_data = json.dumps(r_text, indent=4)
+with open('dict1.json', 'w') as f:
+    f.write(my_data)
