b5a16753b59315a46b59ce5eae6dda22a3791d19
Roovy Shapiro <roovyshapiro@gmail.com>
2019-08-23 14:54:41 -0400

    Add new working API call examples
    VTiger Queries are working properly now
    Understood what Limit and Offset are
    
    The remaining challenge is knowing what account IDs are.

--- VTiger_API.py
+++ VTiger_API.py
@@ -6,21 +6,25 @@ access_key = '(ACCESS KEY)'
 host = 'https://(MYURL).vtiger.com/restapi/v1/vtiger/default'
 
 
-
 #Get information about me
 #url = f"{host}/me"
 
 #Return information about each of the modules within VTiger
 #url = f"{host}/listtypes?fieldTypeList=null"
 
-#Query Case Module and order by Modified Time
-#Returns the first 100 cases
-#url = f"{host}/query?query=Select * FROM Cases order by modifiedtime;"
+#Query Case Module for cases that are not resolved or closed
+#url = f"{host}/query?query=Select * FROM Cases WHERE casestatus != 'closed' AND casestatus != 'resolved' limit 100,200;"
+
+#Returns a count of the items that match the query. In this case, returns the amount of open cases.
+#>>> r_text
+#{'success': True, 'result': [{'count': '229'}]}
+#url = f"{host}/query?query=SELECT COUNT(*) FROM Cases WHERE casestatus != 'closed' AND casestatus != 'resolved';"
+
+#Get information about a module's fields, Cases in this example
+#url = f"{host}/describe?elementType=Employees"
 
 #Return the items from the FAQ module
 #url = f"{host}/query?query=Select * FROM Faq;"
-
-
 
 r = requests.get(url, auth=(username, access_key))
 
