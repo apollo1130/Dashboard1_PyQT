e835255c92c3a3980b1e4fba027c1a35e1d049c4
Roovy Shapiro <roovyshapiro@gmail.com>
2019-08-23 15:09:06 -0400

    Retrieved User/Group IDs
    Used the USERS and GROUPS module to determine everyone's IDs

--- VTiger_API.py
+++ VTiger_API.py
@@ -15,16 +15,17 @@ host = 'https://(MYURL).vtiger.com/restapi/v1/vtiger/default'
 #Query Case Module for cases that are not resolved or closed
 #url = f"{host}/query?query=Select * FROM Cases WHERE casestatus != 'closed' AND casestatus != 'resolved' limit 100,200;"
 
-#Returns a count of the items that match the query. In this case, returns the amount of open cases.
+#Returns a count of the items that match the query. In this case, returns the amount of open cases assigned to the group with the '20x68' group.
 #>>> r_text
 #{'success': True, 'result': [{'count': '229'}]}
-#url = f"{host}/query?query=SELECT COUNT(*) FROM Cases WHERE casestatus != 'closed' AND casestatus != 'resolved';"
+#url = f"{host}/query?query=SELECT COUNT(*) FROM Cases WHERE group_id = '20x68' AND casestatus != 'closed' AND casestatus != 'resolved';"
 
 #Get information about a module's fields, Cases in this example
 #url = f"{host}/describe?elementType=Employees"
 
-#Return the items from the FAQ module
-#url = f"{host}/query?query=Select * FROM Faq;"
+#Return the items from the Users module
+#Use this and GROUPS to get User and Group IDs
+#url = f"{host}/query?query=Select * FROM Users;"
 
 r = requests.get(url, auth=(username, access_key))
 
