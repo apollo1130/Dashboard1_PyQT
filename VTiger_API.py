55c44ea87e340d52c9a6baec968c165a49f9c913
Roovy Shapiro <roovyshapiro@gmail.com>
2019-08-26 15:56:31 -0400

    Add Group / Function Clean Up
    Cleaned up redundancies in code
    Abstracted some functions to reuse code for
    user list and group list

--- VTiger_API.py
+++ VTiger_API.py
@@ -1,37 +1,50 @@
 #https://www.vtiger.com/docs/rest-api-for-vtiger
+#Get information about me
+#url = f"{host}/me"
+#
+#Return information about each of the modules within VTiger
+#url = f"{host}/listtypes?fieldTypeList=null"
+#
+#Query Case Module for cases that are not resolved or closed
+#url = f"{host}/query?query=Select * FROM Cases WHERE casestatus != 'closed' AND casestatus != 'resolved' limit 100,200;"
+#
+#Returns a count of the items that match the query. In this case, returns the amount of open cases assigned to the user with the '19x62' id.
+#>>> r_text
+#{'success': True, 'result': [{'count': '229'}]}
+#url = f"{host}/query?query=SELECT COUNT(*) FROM Cases WHERE assigned_user_id = '19x62' AND casestatus != 'closed' AND casestatus != 'resolved';"
+#
+#Get information about a module's fields, Cases in this example
+#url = f"{host}/describe?elementType=Employees"
+
 import requests, json
 
 username ='(USERNAME)'
 access_key = '(ACCESS KEY)'
 host = 'https://(MYURL).vtiger.com/restapi/v1/vtiger/default'
 
-def get_user_list(host):
+def api_call(url):
     '''
-    Retrieve text of all the user list within VTiger
+    Accepts a URL and returns the text
     '''
-    url = f"{host}/query?query=Select * FROM Users;"    
     r = requests.get(url, auth=(username, access_key))
     print(f"The status code is: {r.status_code}")
     r_text = json.loads(r.text)
     print("Number of Results: ", len(r_text['result']))    
     #Write to a file for backup/review
-    write_to_file(r_text)
-    return r_text
-
-def write_to_file(r_text):
-    '''
-    Write the Text response to a file
-    '''
+    #TODO - Need a better way to write each API call as a separate file
+    #Although this won't be necessary long term anyway. It's mainly for testing.
     my_data = json.dumps(r_text, indent=4)
-    with open('dict1.json', 'w') as f:
+    with open(f"{url[-11:]}.json", 'w') as f:
         f.write(my_data)
-    return None
+        
+    return r_text
 
 def user_dictionary(host):    
     '''
     Accepts User List and returns a dictionary of the username, first, last and id
     '''
-    user_list = get_user_list(host)
+    user_list = api_call(f"{host}/query?query=Select * FROM Users;")
+    
     num_of_users = len(user_list['result'])
     username_list = []
     for user in range(num_of_users):
@@ -42,31 +55,33 @@ def user_dictionary(host):
 
     #Assigns a list of the first name, last name and User ID to the username
     for username in range(num_of_users): 
-        user_dict[username_list[username]] = [user_list['result'][username]['first_name'], user_list['result'][username]['last_name'], user_list['result'][username]['id']]
+        user_dict[username_list[username]] = [user_list['result'][username]['first_name'], user_list['result'][username]['last_name'], user_list['result'][username]['id'], user_list['result'][username]['user_primary_group']]
         
     return user_dict
 
+def group_dictionary(host):    
+    '''
+    Accepts Group List and returns a dictionary of the Group Name and ID
+    '''
+    group_list = api_call(f"{host}/query?query=Select * FROM Groups;")
 
+    num_of_groups = len(group_list['result'])
+    groupname_list = []
+    for group in range(num_of_groups):
+        groupname_list.append(group_list['result'][group]['groupname'])
+        
+    #Creates a dictionary with every group name as the key and an empty list as the value
+    group_dict = {i : [] for i in groupname_list}
+
+    #Assigns a list of the first name, last name and User ID to the username
+    for groupname in range(num_of_groups): 
+        group_dict[groupname_list[groupname]] = group_list['result'][groupname]['id']
+        
+    return group_dict
+
+
+group_dict = group_dictionary(host)
 user_dict = user_dictionary(host)
+for k, v in group_dict.items():
+    print(f"{k}: {v}")  
 
-for k, v in user_dict.items():
-    print(k, v)
-
-
-   
-#Get information about me
-#url = f"{host}/me"
-
-#Return information about each of the modules within VTiger
-#url = f"{host}/listtypes?fieldTypeList=null"
-
-#Query Case Module for cases that are not resolved or closed
-#url = f"{host}/query?query=Select * FROM Cases WHERE casestatus != 'closed' AND casestatus != 'resolved' limit 100,200;"
-
-#Returns a count of the items that match the query. In this case, returns the amount of open cases assigned to the user with the '19x62' id.
-#>>> r_text
-#{'success': True, 'result': [{'count': '229'}]}
-#url = f"{host}/query?query=SELECT COUNT(*) FROM Cases WHERE assigned_user_id = '19x62' AND casestatus != 'closed' AND casestatus != 'resolved';"
-
-#Get information about a module's fields, Cases in this example
-#url = f"{host}/describe?elementType=Employees"
