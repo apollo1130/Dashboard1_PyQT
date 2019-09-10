bee0aeb1dbecb382bfb8bcca5df97bdde2cdb0d6
Roovy Shapiro <roovyshapiro@gmail.com>
2019-09-10 13:59:32 -0400

    Began work on Auto-Refresh
    Performed the intial framework for the auto-refresh button.
    However, trying to call the manual_refresh_button function
    crashes the software. Need to implement PyQT threading
    properly in order for this to work without crashing.

--- app_gui.py
+++ app_gui.py
@@ -23,14 +23,16 @@ class vtiger_api_gui(QtWidgets.QMainWindow, Ui_MainWindow):
 
         self.vtigerapi = VTiger_API.Vtiger_api(self.username, self.access_key, self.host)
 
+        self.manual_refresh_pushButton.clicked.connect(self.manual_refresh_data)
+        self.auto_refresh_checkBox.clicked.connect(self.auto_refresh)
+
         self.week_table.setRowCount(1)
         self.week_table.setCurrentCell(0,0)
         self.week_row = self.week_table.currentRow()
 
         self.today_table.setRowCount(1)
         self.today_table.setCurrentCell(0,0)
-        self.today_row = self.today_table.currentRow()
-        
+        self.today_row = self.today_table.currentRow()        
 
         #Print Silent Errors
         sys._excepthook = sys.excepthook 
@@ -40,9 +42,14 @@ class vtiger_api_gui(QtWidgets.QMainWindow, Ui_MainWindow):
             sys.exit(1) 
         sys.excepthook = exception_hook
 
-        self.manual_refresh_pushButton.clicked.connect(self.manual_refresh_data)
 
     def manual_refresh_data(self):
+        '''
+        This function pulls all the data using the vtigerapi class and fills out
+        the respective fields. It is used by the auto_refresh() function to do this 
+        at regular intervals.
+        '''
+        print('manual refresh data has been called')
         #Total Amount of Open Cases
         case_count = self.vtigerapi.case_count()
         self.total_open_cases_plainTextEdit.setPlainText(case_count)
@@ -84,6 +91,53 @@ class vtiger_api_gui(QtWidgets.QMainWindow, Ui_MainWindow):
                     self.today_table.setRowCount(row_amount + 1) 
                     self.today_row += 1
         self.today_table.setRowCount(row_amount)
+        print('manual refresh data has finished')
+
+
+    def auto_refresh(self):
+        '''
+        This function causes the manual_refresh_data() function to happen at regular intervals.
+        '''
+        if self.refresh_time_lineEdit.text() == '':
+            msg = QtWidgets.QMessageBox()
+            msg.setText("Auto refresh time is empty.\nSelect a refresh time in minutes.")
+            msg.setWindowTitle("Warning!")
+            msg.setIcon(QtWidgets.QMessageBox.Warning)
+            msg.exec_()
+            self.auto_refresh_checkBox.setChecked(False)
+        elif not self.refresh_time_lineEdit.text().isdigit():
+            msg = QtWidgets.QMessageBox()
+            msg.setText("Auto refresh time must be a whole number.\nSelect a refresh time in minutes.")
+            msg.setWindowTitle("Warning!")
+            msg.setIcon(QtWidgets.QMessageBox.Warning)
+            msg.exec_()
+            self.auto_refresh_checkBox.setChecked(False)
+        elif int(self.refresh_time_lineEdit.text()) < 1: 
+            msg = QtWidgets.QMessageBox()
+            msg.setText("Auto refresh time may not be less than 1.\nSelect a refresh time in minutes.")
+            msg.setWindowTitle("Warning!")
+            msg.setIcon(QtWidgets.QMessageBox.Warning)
+            msg.exec_()
+            self.auto_refresh_checkBox.setChecked(False)
+
+        else:
+            #Thread Here for self.manual_refresh_data()
+            timer = int(self.refresh_time_lineEdit.text()) * 30
+            counter = 0
+
+            while True:
+                print(counter)
+                time.sleep(1)
+                counter += 1
+                if counter == timer:
+                    #Thread Here for self.manual_refresh_data()
+                    counter = 0
+                if self.auto_refresh_checkBox.isChecked() == False:
+                    break
+
+        
+
+
 
 
 if __name__ == "__main__":
